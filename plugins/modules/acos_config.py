8ad840bc9cd92bf84845efd80078110baa89bbbc
NehaKembalkarA10 <58077446+NehaKembalkarA10@users.noreply.github.com>
2020-06-15 10:30:08 +0530

    Made minor changes in acos_config

--- plugins/modules/acos_config.py
+++ plugins/modules/acos_config.py
@@ -240,8 +240,8 @@ from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.c
 
 def get_candidate_config(module):
     candidate = ''
-    if module.params["src"]:
-        candidate = module.params["src"]
+    if module.params['src']:
+        candidate = module.params['src']
     elif module.params['lines']:
         candidate_obj = NetworkConfig(indent=1)
         candidate_obj.add(module.params['lines'])
@@ -291,7 +291,7 @@ def main():
         dir_path=dict(type='path')
     )
     argument_spec = dict(
-        src=dict(type="path"),
+        src=dict(type='path'),
         lines=dict(aliases=['commands'], type='list'),
         intended_config=dict(aliases=['commands'], type='list'),
         before=dict(type='list'),
@@ -338,7 +338,7 @@ def main():
         if module.params['backup']:
             result['__backup__'] = contents
 
-    if any((module.params['lines'], module.params["src"])):
+    if any((module.params['lines'], module.params['src'])):
         candidate = get_candidate_config(module)
 
         config_diff = candidate
